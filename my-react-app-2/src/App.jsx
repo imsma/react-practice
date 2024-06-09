140ea2f6dd857171fbd831bc0639bf07be7422b6
SMA <sma@wizlinx.com>
2024-06-09 18:18:58 +0500

    style prop types

--- my-react-app-2/src/App.jsx
+++ my-react-app-2/src/App.jsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <>
       <Student name="Sponger" age={30} isStudent={true} />
+      <Student name="Patrick" age={45} isStudent={false} />
+      <Student name="Squidbard" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student name="Lem" />
     </>
   );
 }
