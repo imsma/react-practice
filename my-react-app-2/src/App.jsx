79c1cd817206baa929c5042868a694662ff47a4e
SMA <sma@wizlinx.com>
2024-06-11 14:33:17 +0500

    Render Lists

--- my-react-app-2/src/App.jsx
+++ my-react-app-2/src/App.jsx
@@ -1,9 +1,30 @@
 import List from "./List.jsx";
 
 function App() {
+  // const fruits = null;
+  const fruits = [
+    { id: 1, name: "apple", calories: 95 },
+    { id: 2, name: "banana", calories: 105 },
+    { id: 3, name: "cherry", calories: 5 },
+    { id: 4, name: "date", calories: 20 },
+    { id: 5, name: "elderberry", calories: 73 },
+  ];
+
+  const vegitables = [
+    { id: 1, name: "potatos", calories: 110 },
+    { id: 2, name: "carrots", calories: 25 },
+    { id: 3, name: "onions", calories: 45 },
+    { id: 4, name: "spinach", calories: 7 },
+    { id: 5, name: "broccoli", calories: 55 },
+  ];
+
   return (
     <>
-      <List />
+      {fruits.length > 0 && <List items={fruits} category="Fruits" />}
+
+      {vegitables.length > 0 && (
+        <List items={vegitables} category="Vegitables" />
+      )}
     </>
   );
 }
--- my-react-app-2/src/List.jsx
+++ my-react-app-2/src/List.jsx
@@ -1,29 +1,42 @@
-function List() {
+function List(props) {
   //const fruits = ["apple", "banana", "cherry", "date", "elderberry"];
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "banana", calories: 105 },
-    { id: 3, name: "cherry", calories: 5 },
-    { id: 4, name: "date", calories: 20 },
-    { id: 5, name: "elderberry", calories: 73 },
-  ];
+  // const fruits = [
+  //   { id: 1, name: "apple", calories: 95 },
+  //   { id: 2, name: "banana", calories: 105 },
+  //   { id: 3, name: "cherry", calories: 5 },
+  //   { id: 4, name: "date", calories: 20 },
+  //   { id: 5, name: "elderberry", calories: 73 },
+  // ];
 
   //   fruits.sort((a, b) => a.name.localeCompare(b.name)); ALPHABETICAL ORDER
   //   fruits.sort((a, b) => b.name.localeCompare(a.name)); // SORT BY CALORIES
   //   fruits.sort((a, b) => a.calories - b.calories); // SORT BY CALORIES
-  fruits.sort((a, b) => b.calories - a.calories); // SORT BY CALORIES
+  // fruits.sort((a, b) => b.calories - a.calories); // SORT BY CALORIES
 
   //   const lowCalFruit = fruits.filter((fruit) => fruit.calories < 100);
 
-  const highCalFruit = fruits.filter((fruit) => fruit.calories >= 100);
+  // const highCalFruit = fruits.filter((fruit) => fruit.calories >= 100);
 
-  const listItems = highCalFruit.map((fruit) => (
-    <li key={fruit.id}>
-      {fruit.name} : &nbsp; <b>{fruit.calories} calories</b>
+  const category = props.category;
+  const itemsList = props.items;
+
+  const listItems = itemsList.map((item) => (
+    <li key={item.id}>
+      {item.name} : &nbsp; <b>{item.calories} calories</b>
     </li>
   ));
 
-  return <ol>{listItems}</ol>;
+  return (
+    <>
+      <h3 className="list-category">{category}</h3>
+      <ol className="list-items">{listItems}</ol>
+    </>
+  );
 }
+
+List.defaultProps = {
+  items: [],
+  category: "Category",
+};
 
 export default List;
