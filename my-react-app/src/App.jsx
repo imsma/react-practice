6ecb558c015b3f7d14a07a73a80179994dbc900c
SMA <sma@wizlinx.com>
2024-06-20 02:28:29 +0500

    feat: Add form input handling and submission functionality

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    address: "",
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+
+    setDetails((d) => {
+      return {
+        ...d,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(details);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h3>Name: </h3> <input name="name" type="text" onChange={handleInput} />
+        <h3>Email: </h3>{" "}
+        <input name="email" type="email" onChange={handleInput} />
+        <h3>Password: </h3>{" "}
+        <input name="password" type="password" onChange={handleInput} />
+        <h3>Address: </h3>{" "}
+        <textarea name="address" onChange={handleInput}></textarea> <br />
+        <button type="submit">Submit Form</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
