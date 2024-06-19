b24fc80cd9fddb285d0d7ed3b7784ab1e303a27f
SMA <sma@wizlinx.com>
2024-06-19 18:50:52 +0500

    useReduce (simple increment)

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,27 @@
+import { useReducer } from "react";
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  return { count: state.count + 1 };
+};
+
 function App() {
-  return <></>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const increaseCount = () => {
+    dispatch();
+  };
+
+  const decreaseCount = () => {};
+
+  return (
+    <>
+      <h2>Count = {state.count}</h2>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </>
+  );
 }
 
 export default App;
