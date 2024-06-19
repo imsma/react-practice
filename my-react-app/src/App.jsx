208038abf9d37cf472ad5db8a1a2ef00765b5577
SMA <sma@wizlinx.com>
2024-06-19 22:26:37 +0500

    without custom hook

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <></>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <>
+      {users.map((user) => {
+        return <h4 key={user.id}>{user.name}</h4>;
+      })}
+    </>
+  );
 }
 
 export default App;
