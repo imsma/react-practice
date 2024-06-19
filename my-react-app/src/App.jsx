2efdc0c552bfcd96b0536b6c7c34170e88faab82
SMA <sma@wizlinx.com>
2024-06-19 22:54:19 +0500

    chore: Update .gitignore and add Axios and dummy data documentation to App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,3 +1,23 @@
+// Axios:
+// Axios is a promise-based HTTP client for the browser and Node.js.
+// It is used to make XMLHttpRequests from the browser
+// that also supports the ES6 Promise API.
+
+// Benefits of Axios:
+// 1. Make XMLHttpRequests from the browser
+// 2. Supports the Promise API
+// 3. Intercept request and response
+// 4. Transform request and response data
+// 5. Cancel requests
+// 6. Automatic transforms for JSON data
+// 7. Client-side support for protecting against XSRF
+
+// Installation:
+// npm install axios
+
+// We will use https://dummyjson.com/ to get dummy data for our project.
+// https://dummyjson.com/docs/products#products-all
+
 function App() {
   return <></>;
 }
