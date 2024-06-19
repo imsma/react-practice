10a719d096042aa9dbeb29d76526e9486f228a10
SMA <sma@wizlinx.com>
2024-06-19 20:00:52 +0500

    1. toggle with useEffect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,24 @@
+/**
+ * useLayoutEffect
+ * useLayoutEffect is a hook that is similar to useEffect.
+ * The difference is that it fires synchronously after all DOM mutations.
+ *
+ */
+
+import { useEffect, useState } from "react";
+
 function App() {
-  return <></>;
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, [toggle]);
+  return (
+    <>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {toggle && <h1>Toggle</h1>}
+    </>
+  );
 }
 
 export default App;
